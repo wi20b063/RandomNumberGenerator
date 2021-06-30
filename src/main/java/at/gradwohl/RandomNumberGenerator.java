8d33cc3dc3f781ed50f00deb2f088d907db571f7
wi20b063 <wi20b063@technikum-wien.at>
2021-06-30 21:05:59 +0200

    Random Number App

--- src/main/java/at/gradwohl/RandomNumberGenerator.java
+++ src/main/java/at/gradwohl/RandomNumberGenerator.java
@@ -1,9 +1,27 @@
+
+
+
 package at.gradwohl;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 public class RandomNumberGenerator {
 
 
 
+        @GET
+        @Produces(MediaType.TEXT_PLAIN)
+        public String calculateUptime() {
+            return "Hello world";
+        }
 
 
-}
+    }
+
+
+
+
